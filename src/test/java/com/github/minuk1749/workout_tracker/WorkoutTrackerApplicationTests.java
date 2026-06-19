ba9d1acc080d50cadce712b0845049bdad2f80a3
minuk1749 <pmn5610@gmail.com>
2026-06-19 20:23:10 +0900

    feat: 초기 설정

---
+++ src/test/java/com/github/minuk1749/workout_tracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.minuk1749.workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
